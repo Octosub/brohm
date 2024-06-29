ab7fe15d8f6c6568d3034cbb13825811c3ec2d7e
Noah Endo <noah.eyeing@gmail.com>
2024-06-29 22:20:41 +0900

    added WebcomponentWrapper to webcomponent and its compiling but no functionality yet

---
+++ modules/resistor/helpers/WebComponentWrapper.tsx
@@ -0,0 +1,26 @@
+import { createElement, useRef, useLayoutEffect, memo } from "react";
+
+const _WebComponentWrapper = (props) => {
+  const { wcTag, children, ...restProps } = props;
+  const wcRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const wc = wcRef.current;
+
+    for (const [key, value] of Object.entries(restProps)) {
+      if (key in wc) {
+        if (wc[key] !== value) {
+          wc[key] = value;
+        }
+      } else {
+        if (wc.getAttribute(key) !== value) {
+          wc.setAttribute(key, value);
+        }
+      }
+    }
+  });
+
+  return createElement(wcTag, {ref: wcRef});
+};
+
+export const WebComponentWrapper = memo(_WebComponentWrapper);
--- modules/resistor/resistorWebComponent.tsx
+++ modules/resistor/resistorWebComponent.tsx
@@ -1,12 +1,35 @@
-//'use client';
+'use client';
+
+import "./resistorWebComponent.css"
 
 class Resistor extends HTMLElement {
+
+    static get observedAttributes() {
+      return ["color"];
+    }
+
+    attributeChangeCallback(name) {
+      if (name === "color") {
+        this.update();
+      }
+    }
+
+    constructor() {
+      super();
+
+      const container = document.createElement("div");
+      this.valSpan = document.createElement("span");
+
+      container.appendChild(this.valSpan);
+    }
+
     connectedCallback() {
-        this.innerHTML = `
-        <div>
-            <h1>Resistor</h1>
-            <p>Resistor comp</p>
-        </div>        `
+      this.appendChild(this.container);
+      this.update();
+    }
+
+    update() {
+      this.valSpan.style.backgroundColor = this.getAttribute("color") || "black";
     }
 }
 
