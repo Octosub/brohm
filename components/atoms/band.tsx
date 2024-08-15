a2fc4114e59bd0ec9b139f412c3b1a734d18c9e2
Noah Endo <noah.eyeing@gmail.com>
2024-08-15 23:17:21 +0900

    change webcompoent to react component

---
+++ components/atoms/band.tsx
@@ -0,0 +1,26 @@
+export interface BandProps {
+  color: string;
+  setColor: (color: string) => void;
+}
+
+export const Band = (props: BandProps) => {
+  return (
+    <select
+      value={props.color}
+      onChange={(e) => {
+        props.setColor(e.target.value)
+        console.log(e.target.value)
+      }}
+    >
+      <option value="black">Black</option>
+      <option value="brown">Brown</option>
+      <option value="red">Red</option>
+      <option value="orange">Orange</option>
+      <option value="yellow">Yellow</option>
+      <option value="green">Green</option>
+      <option value="blue">Blue</option>
+      <option value="purple">Purple</option>
+      <option value="silver">Silver</option>
+    </select>
+  )
+}
